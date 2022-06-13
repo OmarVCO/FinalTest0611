cc37e3ae380403088171f4e306d0facfcfd66d46
OmarVCO <omarsegu@gmail.com>
2022-06-12 20:03:23 -0700

    v3.0

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
-let latlong = {
-  apiKey: "b6c70cdd78857c3fb5f31b62cfb5d8b0",
-  fetchWeather: function () {
-    fetch( 
-      "http://api.openweathermap.org/geo/1.0/direct?q=Vancouver&limit=1&appid=" + this.apiKey
-    ).then((response) => response.json())
-    .then((data) => this.displayLatlong(data));
-  },
-  displayLatlong: function (data) {
-    const { lat } = data[0];
-    const { lon } = data[0];
-    console.log(lat,lon);
-  }
+// let latlong = {
+//   apiKey: "b6c70cdd78857c3fb5f31b62cfb5d8b0",
+//   fetchWeather: function () {
+//     fetch( 
+//       "http://api.openweathermap.org/geo/1.0/direct?q=Vancouver&limit=1&appid=" + this.apiKey
+//     ).then((response) => response.json())
+//     .then((data) => this.displayLatlong(data));
+//   },
+//   displayLatlong: function (data) {
+//     const { lat } = data[0];
+//     const { lon } = data[0];
+//     console.log(lat,lon);
+//   }
+// }
+const API_KEY = 'b6c70cdd78857c3fb5f31b62cfb5d8b0';
+
+getWeatherData ()
+function getWeatherData () {
+navigator.geolocation.getCurrentPosition((success) => {
+    console.log(success);
+
+    let {latitude, longitude } = success.coords;
+    console.log(latitude,longitude);
+
+    fetch( `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&cnt=5&units=metric&appid=${API_KEY}`
+    ).then(res => res.json()).then(data => {
+
+      console.log(data);
+      showWeatherData(data);
+    })
+    
+})
 }
+
+function showWeatherData(data) {
+  let (sunrise, sunset) = data.city;
+  let (humidity, pressure, temp) =  data.list[0].main;
+  let (speed) = data.list[0].wind;
+  console.log(sunrise);
+}
+
+// let otherDayForcast = ''
+// data.daily.forEach(day, idx) =>  {
+//   if(idx == 0) {
+
+//   }else {
+//     otherDayForcast +=`
+//     <div class="weather-forecast" id="weather-forecast">
+//     <div class="weather-forecast-hourly">
+//       <div class="day">0 - 3</div>
+//       <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weather icon" class="w-icon">
+//       <div class="temp">Night - 25.6&#176; C</div>
+//       <div class="temp">Day - 35.6&#176; C</div>
+//     </div>
+//     `
+//   }
+// }
 
 let weather = {
     apiKey: "b6c70cdd78857c3fb5f31b62cfb5d8b0",
     fetchWeather: function () {
       fetch(
-        "https://api.openweathermap.org/data/2.5/forecast?lat=49.2608724&lon=-123.113952&cnt=5&units=metric&appid=b6c70cdd78857c3fb5f31b62cfb5d8b0"
+        "https://api.openweathermap.org/data/2.5/forecast?lat=49.19817697810642&lon=-123.0230679546923&cnt=5&units=metric&appid=b6c70cdd78857c3fb5f31b62cfb5d8b0"
       ).then((response) => response.json())
        .then((data) => this.displayWeather(data));
     },
